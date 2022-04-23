ef58edd6e2f47f38ef22cf3cb868f49aaf54f659
oParaguayo <danielorival123@gmail.com>
2022-04-22 22:16:59 -0300

    Som dos Times

---
+++ js/script.js
@@ -0,0 +1,63 @@
+
+const som1 = document.getElementById('m1')
+const som2 = document.getElementById('m2')
+const som3 = document.getElementById('m3')
+const som4 = document.getElementById('m4')
+const som5 = document.getElementById('m5')
+const som6 = document.getElementById('m6')
+const som7 = document.getElementById('m7')
+const som8 = document.getElementById('m8')
+const som9 = document.getElementById('m9')
+const som10 = document.getElementById('m10')
+
+const img1 = document.getElementById('im1')
+const img2 = document.getElementById('im2')
+const img3 = document.getElementById('im3')
+const img4 = document.getElementById('im4')
+const img5 = document.getElementById('im5')
+const img6 = document.getElementById('im6')
+const img7 = document.getElementById('im7')
+const img8 = document.getElementById('im8')
+const img9 = document.getElementById('im9')
+const img10 = document.getElementById('im10')
+
+img1.addEventListener('click', ()=>{
+    som1.src = "./sons/flamengo.mpeg"
+    m1.play();
+} )
+img2.addEventListener('click', ()=>{
+    som2.src = "./sons/palmeiras.mpeg"
+    m2.play();
+} )
+img3.addEventListener('click', ()=>{
+    som3.src = "./sons/corinthians.mpeg"
+    m3.play();
+} )
+img4.addEventListener('click', ()=>{
+    som4.src = "./sons/atletico.mpeg"
+    m4.play();
+} )
+img5.addEventListener('click', ()=>{
+    som5.src = "./sons/botafogo.mpeg"
+    m5.play();
+} )
+img6.addEventListener('click', ()=>{
+    som6.src = "./sons/vasco.mpeg"
+    m6.play();
+} )
+img7.addEventListener('click', ()=>{
+    som7.src = "./sons/bahia.mpeg"
+    m7.play();
+} )
+img8.addEventListener('click', ()=>{
+    som8.src = "./sons/saopaulo.mpeg"
+    m8.play();
+} )
+img9.addEventListener('click', ()=>{
+    som9.src = "./sons/alemanha.mpeg"
+    m9.play();
+} )
+img10.addEventListener('click', ()=>{
+    som10.src = "./sons/brasil.mpeg"
+    m10.play();
+} )
